30ca2f237f697f6c490861da360def51153e50e5
MorgeMoensch <morgemoensch@morgemoensch.dev>
2023-05-27 20:25:19 +0200

    Finalize v1.0

--- randomverse.js
+++ randomverse.js
@@ -1,3 +1,5 @@
+const maxNumberOfBackgroundImage = 8;
+
 async function getRandomVerse() {
     let response = await fetch(' https://labs.bible.org/api/?passage=random&type=json&formatting=plain');
     let jsonResponse = await response.json();
@@ -16,3 +18,16 @@ function showVerse(book, chapter, verse, text) {
     verseElement.innerText = combinedVerse;
 }
 
+function setRandomBackground() {
+    let body = document.getElementsByTagName('body')[0];
+    body.style.backgroundImage = `url("/assets/background-images/${getRandomInt()}.webp")`
+}
+
+function getRandomInt() {
+    return Math.floor(Math.random() * maxNumberOfBackgroundImage);
+}
+
+window.onload = () => {
+    setRandomBackground();
+    getRandomVerse();
+}
