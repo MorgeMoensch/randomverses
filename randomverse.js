831f9dbf93dfa165618fc53c9156f3b4a9fc2af0
MorgeMoensch <morgemoensch@morgemoensch.dev>
2023-05-27 19:42:52 +0200

    Access verse from API

--- randomverse.js
+++ randomverse.js
@@ -1,3 +1,18 @@
-function getRandomVerse() {
-
+async function getRandomVerse() {
+    let response = await fetch(' https://labs.bible.org/api/?passage=random&type=json&formatting=plain');
+    let jsonResponse = await response.json();
+    let verseJson = jsonResponse[0];
+    showVerse(verseJson.bookname,verseJson.chapter, verseJson.verse, verseJson.text)
 }
+
+function showVerse(book, chapter, verse, text) {
+    console.log(book)
+    let textElement = document.getElementById('verse-text');
+    let verseElement = document.getElementById('verse');
+
+    let combinedVerse = `${book} ${chapter}, ${verse}`;
+
+    textElement.innerText = text;
+    verseElement.innerText = combinedVerse;
+}
+
